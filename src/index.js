416ec3ba394dcdc81cf1ba6d3114add4234bb595
ITigor1985 <xromer85@gmail.com>
2021-08-13 19:52:16 +0300

    2qw

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './sass/main.scss';
-import './js/jquery-3.6.0.min.js';
-import './js/slick/slick.min.js';
-import './js/main'
+
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,25 @@
 
-
-  (function($) {
-    $('.g1').slick();
-     });
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		dots:false,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
