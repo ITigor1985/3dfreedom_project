2dce14237bd0b25282985b12393fb8c21f88a038
ITigor1985 <xromer85@gmail.com>
2021-10-08 16:25:47 +0300

    sdf

--- src/js/problems.js
+++ src/js/problems.js
@@ -1,22 +1,23 @@
 
 
   $(document).ready(function(){
+    
     $('.problems-list').slick({
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
         responsive: [
           {
-            breakpoint: 768,
+            breakpoint: 769,
             settings: {
               arrows: true,
               centerMode: true,
               centerPadding: '40px',
-              slidesToShow: 3
+              slidesToShow: 2
             }
           },
           {
-            breakpoint: 375,
+            breakpoint: 374,
             settings: {
               arrows: true,
               centerMode: true,
@@ -27,3 +28,8 @@
         ]
       });
   });
+  $(document).ready(function(){
+  const prev = document.querySelector(".slick-prev")
+  prev.textContent=`Prev`
+console.log(prev)
+  })
