478854c31a66cbe059e7f27d1691be49cee6351f
Igor Tarasov <xromer85@gmail.com>
2021-10-10 00:12:25 +0300

    123

--- src/js/problems.js
+++ src/js/problems.js
@@ -1,35 +1,33 @@
-
-
-  $(document).ready(function(){
-    
-    $('.problems-list').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 769,
-            settings: {
-              arrows: true,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-               centerMode: true,
-               centerPadding: '30px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+$(document).ready(function () {
+  $('.problems-list').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          centerMode: true,
+          centerPadding: '30px',
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
-  $(document).ready(function(){
-  const prev = document.querySelector(".slick-prev")
-  prev.textContent=`Prev`
-console.log(prev)
-  })
+});
+$(document).ready(function () {
+  const prev = document.querySelector('.slick-prev');
+  prev.textContent = `Prev`;
+  console.log(prev);
+});
