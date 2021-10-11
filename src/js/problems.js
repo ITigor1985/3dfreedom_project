3b22dc4f4ba468b124288364be078da6d240bb0e
Igor Tarasov <xromer85@gmail.com>
2021-10-11 13:02:21 +0300

    problems completed

--- src/js/problems.js
+++ src/js/problems.js
@@ -1,5 +1,7 @@
 import arrow_left from '../images/mobile/problems/arrow_left.png';
 import arrow_right from '../images/mobile/problems/arrow_right.png';
+import arrow_right_desk from '../images/desktop/problems/arrow_right.png';
+import arrow_left_desk from '../images/desktop/problems/arrow_left.png';
 $(document).ready(function () {
   $('.problems-list').slick({
     slidesToShow: 3,
@@ -30,18 +32,45 @@ $(document).ready(function () {
   });
 });
 $(document).ready(function () {
-  const prev = document.querySelector('.slick-prev');
-  prev.innerHTML = '';
-  prev.insertAdjacentHTML(
-    'beforeend',
-    `<img src=${arrow_left} alt="стрелка в лево" class="problems-arrow-left"> `,
-  );
+  // const prev = document.querySelector('.slick-prev');
+  // prev.innerHTML = '';
+  // prev.insertAdjacentHTML(
+  //   'beforeend',
+  //   `<img src=${arrow_left} alt="стрелка в лево" class="problems-arrow-left"> `,
+  // );
+  const width = document.body.clientWidth;
+  if (width > 767) {
+    const prev = document.querySelector('.slick-prev');
+    prev.innerHTML = '';
+    prev.insertAdjacentHTML(
+      'beforeend',
+      `<img src=${arrow_left_desk} alt="стрелка в лево" class="problems-arrow-left"> `,
+    );
+  } else {
+    const prev = document.querySelector('.slick-prev');
+    prev.innerHTML = '';
+    prev.insertAdjacentHTML(
+      'beforeend',
+      `<img src=${arrow_left} alt="стрелка в лево" class="problems-arrow-left"> `,
+    );
+  }
 });
+
 $(document).ready(function () {
-  const next = document.querySelector('.slick-next');
-  next.innerHTML = '';
-  next.insertAdjacentHTML(
-    'beforeend',
-    `<img src=${arrow_right} alt="стрелка в вправо" class="problems-arrow-right"> `,
-  );
+  const width = document.body.clientWidth;
+  if (width > 767) {
+    const next = document.querySelector('.slick-next');
+    next.innerHTML = '';
+    next.insertAdjacentHTML(
+      'beforeend',
+      `<img src=${arrow_right_desk} alt="стрелка в вправо" class="problems-arrow-right"> `,
+    );
+  } else {
+    const next = document.querySelector('.slick-next');
+    next.innerHTML = '';
+    next.insertAdjacentHTML(
+      'beforeend',
+      `<img src=${arrow_right} alt="стрелка в вправо" class="problems-arrow-right"> `,
+    );
+  }
 });
