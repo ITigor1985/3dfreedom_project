ca8e2aaa52483011007b06c8acc01012b0dc3bfa
ITigor1985 <xromer85@gmail.com>
2021-10-18 09:13:58 +0300

    213

--- src/js/before&after.js
+++ src/js/before&after.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-    const width = document.body.clientWidth;
-    if (width <= 767) {
+    
       $('.before-and-after__wrapper').slick({
         dots: true,
         arrows: false,
       });
-    }
+    
   });
