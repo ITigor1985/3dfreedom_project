a3a77866ba1ac9d776b77e47ed75f87ac802f0f1
ITigor1985 <xromer85@gmail.com>
2021-11-04 12:04:08 +0200

    planshet comp

--- src/js/instruments.js
+++ src/js/instruments.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     const width = document.body.clientWidth;
-    if (width <= 767) {
+    if (width <= 1438) {
       $('.instruments-wrapper').slick({
         dots: true,
         arrows: false,
