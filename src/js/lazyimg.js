793c279246c37427cf401be889a2b040ad47f082
Igor Tarasov <xromer85@gmail.com>
2021-11-11 15:45:53 +0300

    123

---
+++ src/js/lazyimg.js
@@ -0,0 +1,20 @@
+const imgGallery = document.querySelectorAll('img');
+imgGallery.forEach(image => {
+  image.classList.add('lazyload');
+  image.setAttribute('data-src', image.src);
+  image.removeAttribute('src');
+  console.log(image);
+});
+
+generateScript();
+
+function generateScript() {
+  const script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+
+  script.integrity =
+    'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+  script.crossOrigin = 'anonymous';
+  script.referrerpolicy = 'no-referrer';
+  document.body.appendChild(script);
+}
