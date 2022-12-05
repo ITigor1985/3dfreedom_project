7f9b96fe30d581523353b98f2ae2e2ac74dd7424
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-12-05 23:43:32 +0200

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,24 @@
 import $ from 'jquery';
-$('a').click(function () {
-  var elementClick = $(this).attr('href');
-  var destination = $(elementClick).offset().top;
-  jQuery('html:not(:animated),body:not(:animated)').animate(
-    {
-      scrollTop: destination,
-    },
-    1000,
-  );
-  return false;
-});
+// $('a').click(function () {
+//   var elementClick = $(this).attr('href');
+//   // var destination = $(elementClick).offset().top;
+//   jQuery('html:not(:animated),body:not(:animated)').animate(
+//     {
+//       scrollTop: destination,
+//     },
+//     1000,
+//   );
+//   return false;
+// });
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn[0].addEventListener('click', toggleModal);
+  refs.openModalBtn[1].addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
