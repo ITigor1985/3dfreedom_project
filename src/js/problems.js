3da3ab062b0ab46fb8e7d5228a40054718ed453d
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-12-24 22:13:23 +0200

    fix

--- src/js/problems.js
+++ src/js/problems.js
@@ -1,7 +1,3 @@
-import arrow_left from '../images/mobile/problems/arrow_left.png';
-import arrow_right from '../images/mobile/problems/arrow_right.png';
-import arrow_right_desk from '../images/desktop/problems/arrow_right.png';
-import arrow_left_desk from '../images/desktop/problems/arrow_left.png';
 import $ from 'jquery';
 $(document).ready(function () {
   $('.problems-list').slick({
@@ -31,47 +27,4 @@ $(document).ready(function () {
       },
     ],
   });
-});
-$(document).ready(function () {
-  // const prev = document.querySelector('.slick-prev');
-  // prev.innerHTML = '';
-  // prev.insertAdjacentHTML(
-  //   'beforeend',
-  //   `<img src=${arrow_left} alt="стрелка в лево" class="problems-arrow-left"> `,
-  // );
-  const width = document.body.clientWidth;
-  if (width > 767) {
-    const prev = document.querySelector('.slick-prev');
-    prev.innerHTML = '';
-    prev.insertAdjacentHTML(
-      'beforeend',
-      `<img src=${arrow_left_desk} alt="стрелка в лево" class="problems-arrow-left"> `,
-    );
-  } else {
-    const prev = document.querySelector('.slick-prev');
-    prev.innerHTML = '';
-    prev.insertAdjacentHTML(
-      'beforeend',
-      `<img src=${arrow_left} alt="стрелка в лево" class="problems-arrow-left"> `,
-    );
-  }
-});
-
-$(document).ready(function () {
-  const width = document.body.clientWidth;
-  if (width > 767) {
-    const next = document.querySelector('.slick-next');
-    next.innerHTML = '';
-    next.insertAdjacentHTML(
-      'beforeend',
-      `<img src=${arrow_right_desk} alt="стрелка в вправо" class="problems-arrow-right"> `,
-    );
-  } else {
-    const next = document.querySelector('.slick-next');
-    next.innerHTML = '';
-    next.insertAdjacentHTML(
-      'beforeend',
-      `<img src=${arrow_right} alt="стрелка в вправо" class="problems-arrow-right"> `,
-    );
-  }
 });
