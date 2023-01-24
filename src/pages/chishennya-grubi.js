d84cbed86d9ba96d7b085c6dfba42cc18bfde553
ITigor1985 <xromer85@gmail.com>
2023-01-24 10:50:34 +0200

    fix

--- src/pages/chishennya-grubi.js
+++
@@ -1,8 +0,0 @@
-import 'modern-normalize/modern-normalize.css';
-import '../sass/main.scss';
-
-import '../js/lazyimg.js';
-import '../js/smoothScroll.js';
-import '../js/header.js';
-import '../js/consultation-form.js';
-import '../js/article.js';
