0175bf7d94fd75a0f339e6032b832419f26435c2
ITigor1985 <xromer85@gmail.com>
2023-01-25 11:26:54 +0200

    page pechi

---
+++ src/chishenny-pechi.js
@@ -0,0 +1,8 @@
+import 'modern-normalize/modern-normalize.css';
+import './sass/main.scss';
+
+import './js/lazyimg.js';
+import './js/smoothScroll.js';
+import './js/header.js';
+import './js/consultation-form.js';
+import './js/article.js';
