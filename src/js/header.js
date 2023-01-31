0ad375a14dae6ec9fd0a3ebc7e77c04c598e5af2
Igor <81867285+ITigor1985@users.noreply.github.com>
2023-01-31 10:16:37 +0200

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 // $('a').click(function () {
 //   var elementClick = $(this).attr('href');
 //   // var destination = $(elementClick).offset().top;
