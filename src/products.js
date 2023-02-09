f16e5dccb105a507f132182979a25659ae5439c5
ITigor1985 <xromer85@gmail.com>
2023-02-09 12:38:05 +0200

    products array

--- src/products.js
+++ src/products.js
@@ -1,24 +1,17 @@
-export const products = [
-  {
-    id: 1,
-    name: 'Полено для чистки дымоходов Hansa Полено-Трубочист(1,1кг)',
-    description: 'Описание для "Футболка черный"',
-    images: ['/images/1/black_front.png', '/images/1/black_back.png'],
-    price: '377 грн.',
-  },
+import { products } from './products-data.js';
 
-  {
-    id: 2,
-    name: 'Средство для чистки дымоходов Sadpal порошок (1кг)',
-    description: 'Средство для чистки дымоходов Sadpal порошок (1кг)',
-    images: ['/images/1/black_front.png', '/images/1/black_back.png'],
-    price: '157 грн.',
-  },
-  {
-    id: 3,
-    name: 'Средство для чистки дымоходов SPALSADZ порошок (1кг)',
-    description: 'Средство для чистки дымоходов SPALSADZ порошок (1кг)',
-    images: ['/images/1/black_front.png', '/images/1/black_back.png'],
-    price: '128 грн.',
-  },
-];
+document.addEventListener('DOMContentLoaded', ready);
+
+function ready() {
+  const ref = {
+    list: document.querySelector('.products__list'),
+  };
+
+  let fragment = document.createDocumentFragment();
+
+  fragment = products.map(item => {
+    return `<li data-card-id =${item.id}><div>${item.name}</div></li>`;
+  });
+
+  ref.list.insertAdjacentHTML('afterbegin', fragment.join(''));
+}
