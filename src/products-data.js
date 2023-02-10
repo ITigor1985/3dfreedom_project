016ac8acc25e45cb98c54c99a78f5b6a5a68941c
ITigor1985 <xromer85@gmail.com>
2023-02-10 11:10:41 +0200

    fix

--- src/products-data.js
+++ src/products-data.js
@@ -3,7 +3,7 @@ export const products = [
     id: 1,
     name: 'Полено для чистки дымоходов Hansa Полено-Трубочист(1,1кг)',
     description: 'Описание для "Футболка черный"',
-    images: ['/images/1/black_front.png', '/images/1/black_back.png'],
+    images: 'poleno',
     price: '377 грн.',
   },
 
@@ -11,14 +11,14 @@ export const products = [
     id: 2,
     name: 'Средство для чистки дымоходов Sadpal порошок (1кг)',
     description: 'Средство для чистки дымоходов Sadpal порошок (1кг)',
-    images: ['/images/1/black_front.png', '/images/1/black_back.png'],
+    images: 'sadpal',
     price: '157 грн.',
   },
   {
     id: 3,
     name: 'Средство для чистки дымоходов SPALSADZ порошок (1кг)',
     description: 'Средство для чистки дымоходов SPALSADZ порошок (1кг)',
-    images: ['/images/1/black_front.png', '/images/1/black_back.png'],
+    images: 'spalsadz',
     price: '128 грн.',
   },
 ];
--- src/products.js
+++ src/products.js
@@ -1,17 +1,43 @@
 import { products } from './products-data.js';
+import img from './images/desktop/products/*.jpg';
+
+const newProducts = products.map(item => {
+  for (const property in img) {
+    if (item.images === property) {
+      item.images = img[property];
+      return item;
+    }
+  }
+});
 
 document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
   const ref = {
-    list: document.querySelector('.products__list'),
+    list: document.querySelector('.list-card'),
   };
-  ref.butt.addEventListener('click', e => console.log(e));
 
   let fragment = document.createDocumentFragment();
 
-  fragment = products.map(item => {
-    return `<li data-card-id =${item.id}><div>${item.name}</div></li>`;
+  fragment = newProducts.map(item => {
+    return `<li class="list-card__item">
+    <div class="list-card__link">
+      <div class="list-card__image-wrapper">      
+
+      <img loading="lazy" src="${item.images}" alt=${item.description} />
+    
+      </div>
+      <div class="list-card__content">
+        <h2 class="list-card__title">
+          ${item.name}
+        </h2>
+        <div class="list-card__pay">
+          <p class="list-card__text">${item.price}</p>
+          <button type="button">Купить</button>
+        </div>
+      </div>
+    </div>
+  </li>`;
   });
 
   ref.list.insertAdjacentHTML('afterbegin', fragment.join(''));
