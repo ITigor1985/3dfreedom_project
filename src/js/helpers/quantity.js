f111f9adc463659b57b9b7322e7195f56d85112a
Igor <81867285+ITigor1985@users.noreply.github.com>
2023-02-16 00:30:33 +0200

    fix

--- src/js/helpers/quantity.js
+++ src/js/helpers/quantity.js
@@ -1,6 +1,15 @@
 export function quantity(str) {
-  console.log(str);
   if (str.match(/[^\d]/) || str.length > 4 || str === '') {
     return 1;
   } else return str;
+}
+
+function phonenumber(inputtxt) {
+  var phoneno = /^d{10}$/;
+  if (inputtxt.value.match(phoneno)) {
+    return true;
+  } else {
+    alert('message');
+    return false;
+  }
 }
