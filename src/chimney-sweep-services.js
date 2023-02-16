084d812d873314f5f5f6310b1f84a0c07ad2b95e
Igor <81867285+ITigor1985@users.noreply.github.com>
2023-02-16 15:36:59 +0200

    add new page chimney-sweep-services

---
+++ src/chimney-sweep-services.js
@@ -0,0 +1,10 @@
+import 'modern-normalize/modern-normalize.css';
+import './sass/main.scss';
+
+import './js/lazyimg.js';
+import './js/smoothScroll.js';
+import './js/header.js';
+import './js/consultation-form.js';
+import './js/article.js';
+import './js/products.js';
+import './js/pay-form.js';
