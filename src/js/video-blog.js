3fd48e3a648116bfb1a8aac4c81038a2c760d134
Igor <81867285+ITigor1985@users.noreply.github.com>
2023-02-21 00:06:15 +0200

    video

--- src/js/video-blog.js
+++ src/js/video-blog.js
@@ -9,26 +9,69 @@ $(document).ready(function () {
   }
 });
 
-window.addEventListener('DOMContentLoaded', function () {
-  let videos = this.document.querySelectorAll('.video');
+// window.addEventListener('DOMContentLoaded', function () {
+//   let videos = this.document.querySelectorAll('.video');
 
-  videos.forEach(video => {
-    video.addEventListener('click', function (e) {
-      const composed = e.composedPath();
-      console.log(composed);
-      if (video.classList.contains('ready')) {
-        return;
-      }
-      video.classList.add('ready');
+//   videos.forEach(video => {
+//     video.addEventListener('click', function (e) {
+//       const composed = e.composedPath();
+//       console.log(composed);
+//       if (video.classList.contains('ready')) {
+//         return;
+//       }
+//       video.classList.add('ready');
 
-      let src = video.dataset.src;
+//       let src = video.dataset.src;
 
-      video.insertAdjacentHTML(
-        'afterbegin',
-        '<iframe         src="' +
-          src +
-          '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;   clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-      );
-    });
-  });
-});
+//       video.insertAdjacentHTML(
+//         'afterbegin',
+//         '<iframe         src="' +
+//           src +
+//           '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;   clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+//       );
+//     });
+//   });
+// });
+
+let videos = document.querySelectorAll('[data-youtube]');
+
+// Progressively enhance them
+for (let video of videos) {
+  // Get the video ID
+  let id = new URL(video.href).searchParams.get('v');
+  // Add the ID to the data-youtube attribute
+  video.setAttribute('data-youtube', id);
+
+  // Add a role of button
+  video.setAttribute('role', 'button');
+
+  // Add a thumbnail
+  video.innerHTML = `<img alt="${video.textContent}" src="https://img.youtube.com/vi_webp/${id}/maxresdefault.webp"><br>
+		${video.textContent}`;
+}
+
+/**
+ * Handle click events on the video thumbnails
+ * @param  {Event} event The event object
+ */
+function clickHandler(event) {
+  // Get the video link
+  let link = event.target.closest('[data-youtube]');
+  if (!link) return;
+
+  // Prevent the URL from redirecting users
+  event.preventDefault();
+
+  // Get the video ID
+  let id = link.getAttribute('data-youtube');
+
+  // Create the player
+  let player = document.createElement('div');
+  player.innerHTML = `<iframe  src="https://www.youtube-nocookie.com/embed/${id}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+  // Inject the player into the UI
+  link.replaceWith(player);
+}
+
+// Detect clicks on the video thumbnails
+document.addEventListener('click', clickHandler);
