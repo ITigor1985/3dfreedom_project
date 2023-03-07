4ffb1f4dc169ef863c595f0f6252c2fc6a3d84d7
Igor <81867285+ITigor1985@users.noreply.github.com>
2023-03-07 13:54:05 +0200

    gallery

---
+++ src/blog.js
@@ -0,0 +1,9 @@
+import 'modern-normalize/modern-normalize.css';
+import './sass/main.scss';
+
+import './js/lazyimg.js';
+import './js/smoothScroll.js';
+import './js/header.js';
+import './js/blog.js';
+import './js/products.js';
+import './js/pay-form.js';
